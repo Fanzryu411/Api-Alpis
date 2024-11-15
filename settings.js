9081e7c41e9163baf37e9551be48633dd38b392d
Fanzryu411 <118971536+Fanzryu411@users.noreply.github.com>
2024-11-15 16:09:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 //_______________________ ┏  Info  ┓ _______________________\\
 const creator = 'Fanz';  // Nama Creator
-port = 8080 //port host
-//const port = process.env.PORT || 8080;  // Port untuk menjalankan server (default: 8080)
+//port = 8080 //port host
+const port = process.env.PORT || 8080;  // Port untuk menjalankan server (default: 8080)
 const LimitApikey = 200;  // Limit Apikey default
 
 //_______________________ ┏  Loghandler  ┓ _______________________\\
